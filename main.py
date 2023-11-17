3f56540ceac4d0c5634b717e541be1edc4035794
Bartosz Oliwa <oliwa.bartosz@gmail.com>
2023-11-17 15:25:01 +0100

    load_env removed

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from dotenv import load_dotenv
-import os
 from flask import Flask, request, jsonify
 
-load_dotenv()
 app = Flask(__name__)
-
 
 @app.route('/openapi', methods=['POST'])
 def post_question():
@@ -15,7 +11,5 @@ def post_question():
 if __name__ == '__main__':
     # Use Gunicorn for production
     host = '0.0.0.0'
-    port = int(os.getenv("PORT", 5000))
-    debug = os.getenv("DEBUG", False)
 
-    app.run(host=host, port=port, debug=debug)
+    app.run(host=host, port=5000, debug=False)
